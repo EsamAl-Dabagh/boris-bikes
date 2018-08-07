628066ee908c75721a0ff700168f28918b835e46
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-07 17:01:27 +0100

    create new bike when release_bike is called

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,11 @@ require 'boris_bikes'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it "checks whether a bike has been created" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike).not_to eq(nil)
+  end
 end
 
 
